d1cf44a25ef5912086348b1ad33788180f1256eb
Seiya Sato <seiya3628@gmail.com>
2019-03-04 02:29:47 +0900

    Add todo.

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,61 @@
 <template>
   <section class="section">
-    <h1>{{ name }}</h1>
+    <h1 class="title">{{ name }}</h1>
+    <div class="columns">
+      <div class="column">
+        <input placeholder="TODO"
+               class="input"
+               @keyup.enter="addTodo"
+        >
+      </div>
+    </div>
+    <div class="columns">
+      <div v-for="todo in todos"
+           :key="todo.id"
+           class="card">
+        <div class="card-header">
+          <p class="card-header-title">
+            <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+            <span :class="{ done: todo.done }">{{ todo.text }}</span>
+          </p>
+          <a class="card-header-icon">
+            <button @click="removeTodo(todo)"
+                    class="button is-danger"
+            >
+              remove
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      name: 'stodo'
+  import { mapMutations } from 'vuex';
+
+  export default {
+    data() {
+      return {
+        name: 'stodo'
+      }
+    },
+    computed: {
+      todos() {
+        return this.$store.state.todos.list
+      }
+    },
+    methods: {
+      removeTodo(todo) {
+        this.$store.commit('todos/remove', todo);
+      },
+      addTodo(e) {
+        this.$store.commit('todos/add', e.target.value)
+        e.target.value = ''
+      },
+      ...mapMutations({
+        toggle: 'todos/toggle'
+      })
     }
   }
-}
 </script>
