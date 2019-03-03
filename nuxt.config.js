0fc7f6fdd5aa24011c7ef7beeff2dcda86042df2
Seiya Sato <seiya3628@gmail.com>
2019-03-04 01:38:40 +0900

    Initialize.

---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    '@nuxtjs/pwa',
+  ],
+  workbox: {
+    // dev: true
+  },
+  manifest: {
+    name: 'STODO',
+    short_name: 'STODO',
+    title: 'STODO',
+    'og:title': 'STODO',
+    description: 'STODO',
+    'og:description': 'STODO',
+    lang: 'ja',
+    theme_color: '#259f94',
+    background_color: '#24130d',
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <h1>{{name}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'stodo'
+    }
+  }
+}
+</script>
