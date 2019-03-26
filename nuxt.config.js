ef0f821dfd6ae7a524ee03b6572288d0d86dc842
Seiya Sato <seiya3628@gmail.com>
2019-03-26 21:44:31 +0900

    Add ogp.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,17 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: pkg.description}
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'STODO' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://stodo.netlify.com/' },
+      { hid: 'og:title', property: 'og:title', content: 'STODO app' },
+      { hid: 'og:description', property: 'og:description', content: 'STODO very simple todo app' },
+      { hid: 'og:image', property: 'og:image', content: 'https://via.placeholder.com/150.jpg' },
+      // { property: 'article:publisher', content: 'FacebookURL' },
+      // { property: 'fb:app_id', content: 'FacebookAppID' },
+      // { name: 'twitter:card', content: 'summary' },
+      // { name: 'twitter:site', content: '@Twitter' },
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
